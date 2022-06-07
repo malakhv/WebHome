7fc06dff12c1e627401c5f1cdf900bffce4fb38c
Mikhail Malakhov <malakhv@gmail.com>
2022-06-07 18:46:47 +0300

    Added an existing version as is

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<!-- The page header -->
+	<head>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+    	<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+    	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    	<title>Mikhan's Home</title>
+	</head>
+
+<body>
+
+	<div id="wrapper">
+		
+		<!-- Include #header div -->
+		<?php include "inc/header.inc"; ?>
+		<!-- Include #menu div -->
+		<?php include "inc/menu.inc"; ?>
+
+        <!-- Page content -->
+        <div id="page"><div id="content">
+            <!-- Welcome -->
+            <?php include "inc/home/welcome.htm"; ?>
+            <!-- About me -->
+            <?php include "inc/home/about_me.htm"; ?>
+            <!-- About this site -->
+            <?php include "inc/home/about_site.htm"; ?>
+        </div><div style="clear: both;">&nbsp;</div></div>
+
+    </div>
+
+    <!-- Include #footer div -->
+    <?php include "inc/footer.htm"; ?>
+
+</body></html>
---
+++ projects.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>xDevStudio - Projects</title>
+		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+		<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	</head>
+
+<body>
+
+	<div id="wrapper">
+		
+		<!-- Include #header div -->
+		<?php include "inc/header.inc"; ?>
+		<!-- Include #menu div -->
+		<?php include "inc/menu.inc"; ?>
+
+		<!-- Page content -->
+		<div id="page"><div id="content">
+			<!-- Include about post -->
+			<?php include "inc/projects/about.inc"; ?>
+			<!-- Include content post -->
+			<?php include "inc/projects/content.inc"; ?>
+		</div><div style="clear: both;">&nbsp;</div></div>
+	
+	</div>
+
+	<!-- Include #footer div -->
+	<?php include "inc/footer.inc"; ?>
+
+</body></html>
