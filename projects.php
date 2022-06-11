69280da4116574f67066f4dbf5143af0d2756de0
Mikhail Malakhov <malakhv@gmail.com>
2022-06-11 16:36:08 +0300

    Refactoring projects section

--- projects.php
+++ projects.php
@@ -2,36 +2,34 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-	<head>
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title>xDevStudio - Projects</title>
-		<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-		<link rel="icon" href="/media/favicon.ico" type="image/x-icon">
+    <head>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>xDevStudio - Projects</title>
+        <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+        <link rel="icon" href="/media/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="/media/favicon.ico" type="image/x-icon">
-	</head>
+    </head>
 
 <body>
 
-	<div id="wrapper">
+    <div id="wrapper">
 		
-		<!-- Include #header div -->
-		<?php include "layout/header.inc"; ?>
-		<!-- Include #menu div -->
-		<?php include "layout/menu.inc"; ?>
+        <!-- Include #header div -->
+        <?php include "layout/header.inc"; ?>
+        <!-- Include #menu div -->
+        <?php include "layout/menu.inc"; ?>
 
-		<!-- Page content -->
-		<div id="page"><div id="content">
-			<!-- Include about post -->
-			<?php include "content/projects/about.htm"; ?>
-			<!-- Include content post -->
-			<?php include "content/projects/content.htm"; ?>
-		</div><div style="clear: both;">&nbsp;</div></div>
+        <!-- Page content -->
+        <div id="page"><div id="content">
+            <!-- Include content post -->
+            <?php include "content/projects/content.htm"; ?>
+        </div><div style="clear: both;">&nbsp;</div></div>
 	
-	</div>
+    </div>
 
-	<!-- Include #footer div -->
-	<?php include "layout/footer.inc"; ?>
+    <!-- Include #footer div -->
+    <?php include "layout/footer.inc"; ?>
 
 </body></html>
