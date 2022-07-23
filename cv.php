342225daef6dedea37b7c14ef6c5712932c26d47
Mikhail Malakhov <malakhv@gmail.com>
2022-07-23 16:37:13 +0300

    Added CV page

---
+++ cv.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Mikhail - Curriculum Vitae</title>
+        <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+        <link rel="icon" href="/media/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/media/favicon.ico" type="image/x-icon">
+    </head>
+
+<body>
+
+    <div id="wrapper">
+		
+        <!-- Include #header div -->
+        <?php include "layout/header.htm"; ?>
+        <!-- Include #menu div -->
+        <?php include "layout/menu.htm"; ?>
+
+        <!-- Page content -->
+        <div id="page"><div id="content">
+            <!-- Include content post -->
+            <?php include "content/home/cv.htm"; ?>
+            <?php include "content/stub.htm"; ?>
+        </div><div style="clear: both;">&nbsp;</div></div>
+	
+    </div>
+
+    <!-- Include footer -->
+    <?php include "layout/footer.htm"; ?>
+
+</body></html>
