29f0a5a5011b9744dbc7ea2c2329b2ee4cc20d33
Mikhail Malakhov <malakhv@gmail.com>
2022-08-06 17:41:22 +0300

    Added green theme

--- layout/layout.php
+++ layout/layout.php
@@ -2,12 +2,13 @@
 
 # Make a page head with specific title, theme, keywords and description
 function MakeHtmlHead($title, $keywords = '', $description = '') {
-    $theme = "lite";
+    $theme = "green";
     echo '<head>';
     echo '<meta name="keywords" content="'.$keywords.'" />';
     echo '<meta name="description" content="'.$description.'" />';
     echo '<meta http-equiv="content-type" content="text/html; charset=utf-8" />';
     echo '<link href="layout/'.$theme.'/style.css" rel="stylesheet" type="text/css" media="screen" />';
+    echo '<link href="layout/'.$theme.'/mobile.css" rel="stylesheet" type="text/css" media="screen and (max-width: 900px)">';
     echo '<link rel="icon" href="/media/favicon.png" type="image/png">';
     echo '<link rel="shortcut icon" href="/media/favicon.png" type="image/png">';
     echo '<title>'.$title.'</title>';
@@ -16,7 +17,7 @@ function MakeHtmlHead($title, $keywords = '', $description = '') {
 
 # Make page header (header and menu)
 function MakePageHeader() {
-    $theme = "lite";
+    $theme = "green";
     // Include #header div
     include 'layout/'.$theme.'/header.htm';
     // Include #menu div
@@ -25,7 +26,7 @@ function MakePageHeader() {
 
 # Make page footer
 function MakePageFooter() {
-    $theme = "lite";
+    $theme = "green";
     // Include footer
     include 'layout/'.$theme.'/footer.htm';
 }
