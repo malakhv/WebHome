2de18a1c153fdfaa8977612cf6c744fab2fa351c
Mikhail Malakhov <malakhv@gmail.com>
2023-07-26 20:39:32 +0300

    Added empty pages

---
+++ new/media/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+    <!-- The page html header -->
+    <?php MakeHtmlHead('Mikhail Malakhov - Projects'); ?>
+
+<body><div id="content">
+
+    <!-- Header -->
+    <?php InsertPageHeader(); ?>
+    <!-- Menu -->
+    <?php InsertMenu(); ?>
+
+    <!-- Page content -->
+    <?php
+        IncludeContent('develop/content.htm');
+    ?>
+
+    <!-- Footer -->
+    <?php InsertPageFooter(); ?>
+
+</div></body></html>
