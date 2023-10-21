51282857e9028546f73a3177768f617fa4abd47a
Mikhail Malakhov <malakhv@gmail.com>
2023-10-21 15:57:59 +0300

    Added Notepad

---
+++ notepad/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php MakeHtmlHead(getPageTitle()); ?>
+
+<body><div id="content">
+
+<?php
+    //InsertPageHeader();
+    //InsertMenu();
+    IncludeContent('notepad/content.htm');
+    //ApplyContent();
+?>
+
+<?php
+    InsertPageFooter();
+?>
+
+</div></body></html>
